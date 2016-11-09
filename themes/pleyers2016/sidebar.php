5580d70901f86bdb8b5ca747386adfd73690ee1b
helagone <luah613@gmail.com>
2016-11-09 07:44:15 -0600

    fehca hace cuanto sidebar

--- themes/pleyers2016/sidebar.php
+++ themes/pleyers2016/sidebar.php
@@ -3,13 +3,14 @@
 		<?php 
 
 				$args = array(
-							'post_type'=>'sprints',
-							'posts_per_page'=>-1,
+							'post_type'=>'post',//cambiar por el posttype -> sprints
+							'posts_per_page'=>15,
 							'post_status'=>'publish',
 							'orderby'=>'date',
 							'order'=>'DESC'
 							);
 				$posts = new WP_Query($args);
+				$hoy = date('U');
 		?>
 				<h3 class="header_sprints">SPRINTS</h3>
 				<div class="sprints_container">
@@ -17,9 +18,30 @@
 						if($posts->have_posts()): 
 							while($posts->have_posts()):
 								$posts->the_post();
+								$post_date = get_the_date('U');
+								$resta = $hoy - $post_date;
+								$segundos = $resta;
+								$minutos = $segundos / 60;
+								$horas = $minutos / 60;
+								$dias = $horas / 24;
+								$meses = $dias / 30;
+
+								//echo "m> ".round($minutos)." h> ".round($horas)." d> ".round($dias)." M> ".round($meses)."<br>";
+								//echo date('F j, Y', $post_date);
+							if($segundos > 60 && $minutos < 60){
+								$hace = round($minutos)."m";
+							}else if($minutos > 60 && $horas < 24){
+								$hace = round($horas)."h";
+							}else if($horas > 24 && $dias < 30){
+								$hace = round($dias)."d";
+							}else if($dias > 30 && $meses < 12){
+								$hace = round($meses)."M";
+							}
+								
+
 					?>
 					<div class="formato_b sprints_post clearfix">
-						<span class="post_time">2h</span>
+						<span class="post_time"><?php  echo $hace; ?></span>
 						<div class="sprints_post_content">
 							<a href="">
 								<div class="img_frame">
@@ -29,7 +51,7 @@
 							</a>
 							<a href="<?php the_permalink(); ?>">
 								<p>
-									<?php the_content(); ?>
+									<?php the_title(); //the_content(); ?>
 								</p>
 							</a>
 						</div>
