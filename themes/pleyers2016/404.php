22e1fea5dfefaae75ef1956eabc5fc18c0ecb8ef
helagone <luah613@gmail.com>
2016-11-10 18:00:00 -0600

    404 & error search

--- themes/pleyers2016/404.php
+++ themes/pleyers2016/404.php
@@ -1,157 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Page Not Found :(</title>
-        <style>
-            ::-moz-selection {
-                background: #b3d4fc;
-                text-shadow: none;
-            }
+<?php get_header(); ?>
+        <div class="container clearfix">
+            <section class="single_post">
 
-            ::selection {
-                background: #b3d4fc;
-                text-shadow: none;
-            }
+                <div class="top_taxonomy">
+                    <div class="tax_description">
+                        <h2>Error 404</h2>
+                        <span>No encontramos lo que estas buscando<br>pero esto podría interesarte.</span>
+                    </div>
+                
+                </div><!-- top_taxonomy -->
 
-            html {
-                padding: 30px 10px;
-                font-size: 20px;
-                line-height: 1.4;
-                color: #737373;
-                background: #f0f0f0;
-                -webkit-text-size-adjust: 100%;
-                -ms-text-size-adjust: 100%;
-            }
+                <div class="posts_pool clearfix">
+                <?php
+                    $types = get_all_posttypes();
+                    $args = array(
+                                'post_type'=>$types,
+                                'posts_per_page'=>10,
+                                'post_status'=>'publish',
+                                'orderby'=>'rand',
+                                'order'=>'DESC'
+                                );
+                    $posts = new WP_Query($args);
+                    // echo "<pre>";
+                    //     print_r($posts);
+                    // echo "</pre>";
+                        if($posts->have_posts()): 
+                            while($posts->have_posts()):
+                                $posts->the_post(); setup_postdata($post);
+                                $tags = get_the_tags();
 
-            html,
-            input {
-                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-            }
-
-            body {
-                max-width: 500px;
-                _width: 500px;
-                padding: 30px 20px 50px;
-                border: 1px solid #b3b3b3;
-                border-radius: 4px;
-                margin: 0 auto;
-                box-shadow: 0 1px 10px #a7a7a7, inset 0 1px 0 #fff;
-                background: #fcfcfc;
-            }
-
-            h1 {
-                margin: 0 10px;
-                font-size: 50px;
-                text-align: center;
-            }
-
-            h1 span {
-                color: #bbb;
-            }
-
-            h3 {
-                margin: 1.5em 0 0.5em;
-            }
-
-            p {
-                margin: 1em 0;
-            }
-
-            ul {
-                padding: 0 0 0 40px;
-                margin: 1em 0;
-            }
-
-            .container {
-                max-width: 380px;
-                _width: 380px;
-                margin: 0 auto;
-            }
-
-            /* google search */
-
-            #goog-fixurl ul {
-                list-style: none;
-                padding: 0;
-                margin: 0;
-            }
-
-            #goog-fixurl form {
-                margin: 0;
-            }
-
-            #goog-wm-qt,
-            #goog-wm-sb {
-                border: 1px solid #bbb;
-                font-size: 16px;
-                line-height: normal;
-                vertical-align: top;
-                color: #444;
-                border-radius: 2px;
-            }
-
-            #goog-wm-qt {
-                width: 220px;
-                height: 20px;
-                padding: 5px;
-                margin: 5px 10px 0 0;
-                box-shadow: inset 0 1px 1px #ccc;
-            }
-
-            #goog-wm-sb {
-                display: inline-block;
-                height: 32px;
-                padding: 0 10px;
-                margin: 5px 0 0;
-                white-space: nowrap;
-                cursor: pointer;
-                background-color: #f5f5f5;
-                background-image: -webkit-linear-gradient(rgba(255,255,255,0), #f1f1f1);
-                background-image: -moz-linear-gradient(rgba(255,255,255,0), #f1f1f1);
-                background-image: -ms-linear-gradient(rgba(255,255,255,0), #f1f1f1);
-                background-image: -o-linear-gradient(rgba(255,255,255,0), #f1f1f1);
-                -webkit-appearance: none;
-                -moz-appearance: none;
-                appearance: none;
-                *overflow: visible;
-                *display: inline;
-                *zoom: 1;
-            }
-
-            #goog-wm-sb:hover,
-            #goog-wm-sb:focus {
-                border-color: #aaa;
-                box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-                background-color: #f8f8f8;
-            }
-
-            #goog-wm-qt:hover,
-            #goog-wm-qt:focus {
-                border-color: #105cb6;
-                outline: 0;
-                color: #222;
-            }
-
-            input::-moz-focus-inner {
-                padding: 0;
-                border: 0;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Not found <span>:(</span></h1>
-            <p>Sorry, but the page you were trying to view does not exist.</p>
-            <p>It looks like this was the result of either:</p>
-            <ul>
-                <li>a mistyped address</li>
-                <li>an out-of-date link</li>
-            </ul>
-            <script>
-                var GOOG_FIXURL_LANG = (navigator.language || '').slice(0,2),GOOG_FIXURL_SITE = location.host;
-            </script>
-            <script src="//linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js"></script>
+                    ?>
+                    <div class="post clearfix">
+                        <a href="">
+                            <div class="img_frame clearfix">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+                        </a>
+                        <?php 
+                            $terms = wp_get_post_terms($post->ID, 'noticiasde' ); 
+                            if($terms){
+                                foreach($terms as $term):
+                            ?>
+                                <a href="<?php echo bloginfo('url').'/noticiasde/'.$term->slug; ?>">
+                                    <span>
+                                        <?php echo "#".esc_html($term->name)." "; ?>
+                                    </span>
+                                </a>    
+                            <?php       
+                            endforeach; }
+                        ?>
+                        <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
+                    </div>
+                <?php
+                                wp_reset_postdata(); 
+                            endwhile; 
+                        endif; 
+                    ?>
+                </div>
+            </section>
         </div>
-    </body>
-</html>
+    <?php get_footer(); ?>
+<?php get_footer(); ?>
--- themes/pleyers2016/search.php
+++ themes/pleyers2016/search.php
@@ -2,6 +2,7 @@
 	get_header(); 
 	$query = $_GET['s'];
 	$total_results = $wp_query->found_posts;
+	if(!empty($total_results)){
 ?>
 	<div class="container clearfix">
 		<section class="single_post">
@@ -13,47 +14,102 @@
 			
 			</div><!-- top_taxonomy -->
 			<div class="posts_pool clearfix">
-			<?php
-				if(!empty($total_results)){
-					if(have_posts()): 
-						while(have_posts()):
-							the_post(); setup_postdata($post);
-							$tags = get_the_tags();
+				<?php
+						if(have_posts()): 
+							while(have_posts()):
+								the_post(); setup_postdata($post);
+								$tags = get_the_tags();
 
-				?>
-				<div class="post clearfix">
-					<a href="">
-						<div class="img_frame clearfix">
-							<?php the_post_thumbnail(); ?>
-						</div>
-					</a>
-					<?php 
-						$terms = wp_get_post_terms($post->ID, 'noticiasde' ); 
-						if($terms){
-							foreach($terms as $term):
-						?>
-							<a href="<?php echo bloginfo('url').'/noticiasde/'.$term->slug; ?>">
-								<span>
-									<?php echo "#".esc_html($term->name)." "; ?>
-								</span>
-							</a>	
-						<?php		
-						endforeach; }
 					?>
-					<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
-				</div>
-			<?php
-							wp_reset_postdata(); 
-						endwhile; 
-					endif; 
-				}else{
-				?>
-					<h2>Ooops!</h2>
-					<p>No encontramos lo que estás buscando, inténtalo de nuevo por favor.</p>
-			<?php
-				}
-				?>
+								<div class="post clearfix">
+									<a href="">
+										<div class="img_frame clearfix">
+											<?php the_post_thumbnail(); ?>
+										</div>
+									</a>
+									<?php 
+										$terms = wp_get_post_terms($post->ID, 'noticiasde' ); 
+										if($terms){
+											foreach($terms as $term):
+										?>
+											<a href="<?php echo bloginfo('url').'/noticiasde/'.$term->slug; ?>">
+												<span>
+													<?php echo "#".esc_html($term->name)." "; ?>
+												</span>
+											</a>	
+										<?php		
+										endforeach; }
+									?>
+									<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
+								</div>
+				<?php
+								wp_reset_postdata(); 
+							endwhile; 
+						endif; 
+					?>
 			</div>
 		</section>
 	</div>
+			<?php		
+				}else{
+				?>
+					<div class="container clearfix">
+			            <section class="single_post">
+
+			                <div class="top_taxonomy">
+			                    <div class="tax_description">
+			                        <h2>Error 404</h2>
+			                        <span>No encontramos lo que estas buscando<br>pero esto podría interesarte.</span>
+			                    </div>
+			                
+			                </div><!-- top_taxonomy -->
+
+			                <div class="posts_pool clearfix">
+				                <?php
+				                    $types = get_all_posttypes();
+				                    $args = array(
+				                                'post_type'=>$types,
+				                                'posts_per_page'=>10,
+				                                'post_status'=>'publish',
+				                                'orderby'=>'rand',
+				                                'order'=>'DESC'
+				                                );
+				                    $posts = new WP_Query($args);
+				                        if($posts->have_posts()): 
+				                            while($posts->have_posts()):
+				                                $posts->the_post(); setup_postdata($post);
+				                                $tags = get_the_tags();
+				                    ?>
+							                    <div class="post clearfix">
+							                        <a href="">
+							                            <div class="img_frame clearfix">
+							                                <?php the_post_thumbnail(); ?>
+							                            </div>
+							                        </a>
+							                        <?php 
+							                            $terms = wp_get_post_terms($post->ID, 'noticiasde' ); 
+							                            if($terms){
+							                                foreach($terms as $term):
+							                            ?>
+							                                <a href="<?php echo bloginfo('url').'/noticiasde/'.$term->slug; ?>">
+							                                    <span>
+							                                        <?php echo "#".esc_html($term->name)." "; ?>
+							                                    </span>
+							                                </a>    
+							                            <?php       
+							                            endforeach; }
+							                        ?>
+							                        <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
+							                    </div>
+				                <?php
+				                                wp_reset_postdata(); 
+				                            endwhile; 
+				                        endif; 
+				                    ?>
+			                </div>
+			            </section>
+			        </div>
+<?php
+	}
+	?>
 <?php get_footer(); ?>
